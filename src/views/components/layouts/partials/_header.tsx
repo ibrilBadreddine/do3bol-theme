2b8b2af0f8db84ba48029daa44433e5742c3b8ab
Badreddine Ibril <badreddine.ibril@youcan.shop>
2024-10-29 15:24:49 +0100

    Handle the "header" component

--- src/views/components/layouts/partials/_header.tsx
+++ src/views/components/layouts/partials/_header.tsx
@@ -1,3 +1,69 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import StoreLogo from "@/assets/images/logo.png";
+import Icon from "@components/shared/icons";
+
 export default function Header() {
-  return <header className="header-container">Header</header>;
+  const [search, setSearch] = useState(false);
+  const [menu, setMenu] = useState(false);
+
+  return (
+    <header className="header-container">
+      <div className="links-box">
+        <Link to="/">Home</Link>
+        <Link to="/">Man</Link>
+        <Link to="/">Woman</Link>
+        <Link to="/">Kids</Link>
+      </div>
+      <div className="header-mobile" data-hidden={!menu}>
+        <button onClick={() => setMenu(true)} className="menu-btn icon">
+          <Icon name="burger_menu" size={20} />
+        </button>
+        <div className="menu-container">
+          <div className="menu-box">
+            <div className="menu-head">
+              <img src={StoreLogo} alt="store logo" />
+              <button onClick={() => setMenu(false)} className="icon">
+                <Icon name="close" />
+              </button>
+            </div>
+            <div className="menu-core">
+              <input type="search" placeholder="Search..." />
+              <div className="links">
+                <Link to="/">
+                  Home <Icon name="arrow_right_up" />
+                </Link>
+                <Link to="/">
+                  Man <Icon name="arrow_right_up" />
+                </Link>
+                <Link to="/">
+                  Woman <Icon name="arrow_right_up" />
+                </Link>
+                <Link to="/">
+                  Kids <Icon name="arrow_right_up" />
+                </Link>
+              </div>
+            </div>
+          </div>
+          <div className="menu-overlay overlay" />
+        </div>
+      </div>
+      <Link to="/" className="logo-box">
+        <img src={StoreLogo} alt="store logo" />
+      </Link>
+      <div className="actions-box">
+        <div className="search-bar" data-hidden={!search}>
+          <Icon name="search" />
+          <input type="search" placeholder="Search..." />
+        </div>
+        <button onClick={() => setSearch(!search)} className="icon search-btn">
+          <Icon name={!search ? "search" : "close"} />
+        </button>
+        <button className="icon cart-btn">
+          <Icon name="cart" />
+          <span>0 MAD</span>
+        </button>
+      </div>
+    </header>
+  );
 }
---
+++ src/views/components/shared/icons/index.tsx
@@ -0,0 +1,115 @@
+import React from "react";
+
+const icons = {
+  arrow_right: (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className="size-6">
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3"
+      />
+    </svg>
+  ),
+  arrow_right_up: (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className="size-6">
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25"
+      />
+    </svg>
+  ),
+  burger_menu: (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className="size-6">
+      <path
+        d="M5 8H13.75M5 12H19M10.25 16L19 16"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  ),
+  cart: (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className="size-6">
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z"
+      />
+    </svg>
+  ),
+  close: (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className="size-6">
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M6 18 18 6M6 6l12 12"
+      />
+    </svg>
+  ),
+  search: (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className="size-6">
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
+      />
+    </svg>
+  ),
+};
+
+interface IconProps extends React.SVGProps<SVGSVGElement> {
+  name: keyof typeof icons;
+  size?: number;
+}
+
+const Icon: React.FC<IconProps> = ({ name, size = 18, ...rest }) => {
+  const icon = icons[name];
+
+  if (!icon) {
+    return null;
+  }
+
+  return React.cloneElement(icon, {
+    width: size,
+    height: size,
+    ...rest,
+  });
+};
+
+export default Icon;
