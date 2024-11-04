11a3ed3dabdc4d9f0494b991c2ed8e1cf427b7fb
Badreddine Ibril <badreddine.ibril@youcan.shop>
2024-11-04 16:45:38 +0100

    Search product > add empty state

--- src/views/pages/search/partials/result.tsx
+++ src/views/pages/search/partials/result.tsx
@@ -1,9 +1,15 @@
-import ProductsList from "@/views/components/sections/products/list";
+import ProductsList from "@components/sections/products/list";
+import Empty from "@components/shared/empty";
 
 export default function Result() {
   return (
     <div className="result-box">
       <ProductsList gap={1} length={6} />
+      {/* <Empty
+        title="No products found!"
+        subtitle="Your search didn't match any product, Please try again."
+        icon="search"
+      /> */}
     </div>
   );
 }
