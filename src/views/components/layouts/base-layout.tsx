1ce531a4b1b387a03c48c5d5c48ccfebc142cdae
Badreddine Ibril <badreddine.ibril@youcan.shop>
2024-11-05 14:30:55 +0100

    Navigation fix > scroll top

--- src/views/components/layouts/base-layout.tsx
+++ src/views/components/layouts/base-layout.tsx
@@ -1,9 +1,15 @@
 import "./style.scss";
 import Header from "./partials/_header";
 import Footer from "./partials/_footer";
-import { Outlet } from "react-router-dom";
+import { useLayoutEffect } from "react";
+import { Outlet, useLocation } from "react-router-dom";
 
 export default function Layout() {
+  const location = useLocation();
+
+  useLayoutEffect(() => {
+    document.documentElement.scrollTo({ top: 0, left: 0, behavior: "instant" });
+  }, [location.pathname]);
   return (
     <div className="base-app">
       <Header />
