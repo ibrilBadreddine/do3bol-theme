4132b15dab7f87a71ab6a44a6b69a06bfcf37030
Badreddine Ibril <badreddine.ibril@youcan.shop>
2024-11-05 14:19:41 +0100

    Error page > add translation

--- src/views/pages/errors/404/index.tsx
+++ src/views/pages/errors/404/index.tsx
@@ -1,22 +1,23 @@
 import "../style.scss";
 import { Link } from "react-router-dom";
 import Icon from "@/views/components/shared/icons";
+import { useTranslation } from "react-i18next";
 
 export default function NotFound() {
+  const { t } = useTranslation();
+
   return (
     <div className="not-found-page">
       <div className="not-found-box">
         <div className="box-content">
-          <h1 className="box-error">404</h1>
-          <h1 className="box-title">Oops! Page Not Found</h1>
-          <p className="box-subtitle">
-            The page you're looking for doesn't exist or has been moved.
-          </p>
+          <h1 className="box-error">{t("pages.not-found.code")}</h1>
+          <h1 className="box-title">{t("pages.not-found.title")}</h1>
+          <p className="box-subtitle">{t("pages.not-found.subtitle")}</p>
         </div>
         <div className="box-actions">
           <Link to="/" className="back-link">
             <Icon name="arrow_left" />
-            Go back
+            {t("pages.not-found.action")}
           </Link>
         </div>
       </div>
