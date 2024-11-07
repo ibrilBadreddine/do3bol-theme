9fc3509388a31b11a3c943cfcbca59f0bd7e43c8
Badreddine Ibril <badreddine.ibril@youcan.shop>
2024-11-07 11:10:27 +0100

    Dynamic language switch

--- src/main.tsx
+++ src/main.tsx
@@ -2,5 +2,10 @@ import "@/assets/style/main.scss";
 import { createRoot } from "react-dom/client";
 import App from "@/routes/router";
 import "@/core/lang/i18n";
+import { CustomizationProvider } from "./core/context/custom";
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <CustomizationProvider>
+    <App />
+  </CustomizationProvider>
+);
